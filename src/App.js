8c064010454409afb9962dc47f7afc2a810e6698
mwr2019 <47264239+mwr2019@users.noreply.github.com>
2020-01-31 11:05:57 +0100

    initial release

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.css';
 import ButtonAppBar from "./Components/topBar";
 import SimpleContainer from "./Components/contaier1";
-import ContainedButtons from "./Components/buttonone";
 import SimpleContainer2 from "./Components/container2";
 
 function App() {
@@ -10,8 +9,7 @@ function App() {
       <div>
    <ButtonAppBar/>
    <SimpleContainer/>,
-   <SimpleContainer2/>
-       <ContainedButtons/>,
+   <SimpleContainer2/>,
       </div>
   );
 }
